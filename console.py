e7e750879b70419276d2929e27d0dc0d359c5da0
mag389 <mag389@cornell.edu>
2020-08-12 15:57:14 +0000

    removed badly commented line

--- console.py
+++ console.py
@@ -115,7 +115,6 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        """ check if args exist if so split them into parts"""
         if not args:
             print("** class name missing **")
             return
