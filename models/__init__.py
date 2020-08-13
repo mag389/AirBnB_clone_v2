353e4b8ddcd4e0570e13f4c93d6e611f6f38e816
mag389 <mag389@cornell.edu>
2020-08-13 22:15:18 +0000

    chooses different storage engine

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+import os
 
 
-storage = FileStorage()
-storage.reload()
+store_type = os.environ.get('HBNB_TYPE_STORAGE')
+print("in the init")
+print(store_type)
+if store_type == "db":
+    """ use the mysql database storage version """
+    print("db type storage")
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    """ use the default json storage """
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
