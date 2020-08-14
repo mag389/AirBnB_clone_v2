16bd359d461a991963456ba2917cee35bedc416f
mag389 <mag389@cornell.edu>
2020-08-14 02:24:27 +0000

    cities getter method for state

--- models/state.py
+++ models/state.py
@@ -10,3 +10,16 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete", backref="state")
+
+    @property
+    def cities(self):
+        """return list of cities with this state id """
+        from models.city import City
+        from models.__init__ import storage
+        from models.engine.file_storage import FileStorage
+        cits = FileStorage.all(City)
+        cits_b = []
+        for city in cits:
+            if city.state_id == self.id:
+                cits_b.append(city)
+        return cits_b
