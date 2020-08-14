df93133506d95036f0f0ed41fb5326917452a212
CARTER_CLEMENTS <831@holbertonschool.com>
2020-08-13 21:23:08 -0400

    Updated to support SQL engine

--- models/city.py
+++ models/city.py
@@ -3,7 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
@@ -11,3 +12,5 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', cascade='all, delete',
+                          backref='cities')
