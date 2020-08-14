228944dea586ede1e8b2fc788ebbdee52c7064ba
mag389 <mag389@cornell.edu>
2020-08-14 01:11:29 +0000

    wrapped kwargs check in an if statement

--- models/base_model.py
+++ models/base_model.py
@@ -24,10 +24,13 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            stringy = "%Y-%m-%dT%H:%M:%S.%f"
+            if "updated_at" in kwargs.keys():
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         stringy)
+            if "created_at" in kwargs.keys():
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         stringy)
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
