b0b15ef1c6458267cdeef377641a2c6e345d5b5d
CARTER_CLEMENTS <831@holbertonschool.com>
2020-08-13 22:47:16 -0400

    Fix typo

--- models/place.py
+++ models/place.py
@@ -22,7 +22,3 @@ class Place(BaseModel, Base):
 
     reviews = relationship('Review', backref='place',
                            cascade='all, delete')
-    amenities = relationship('Amenity',
-                             secondary=place_amenity,
-                             backref='place_amenities'
-                             viewonly=False)
