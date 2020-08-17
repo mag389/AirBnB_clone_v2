fc5f203afbdac2571cef1b116b8e29827d3cfb2a
mag389 <mag389@cornell.edu>
2020-08-17 20:55:32 +0000

    make tgz from web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ packing all files """
 from fabic.api impoort *
+from datetime import datetime
+import os
 
 
 def do_pack():
     """ packs all files to a tgz file from web_static folder"""
-    
+    now = datetime.now()
+    dt = now.strftime("%Y%m%d%H%M%S")
+    fname = "web_static_" + dt + ".tgz"
+    local("mkdir -p versions")
+    local("tar -cvf {} web_static".format(fname))
+    if os.path.exists(fname):
+        return fname
+    return None
