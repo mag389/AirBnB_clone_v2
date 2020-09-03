0eb0504d765caf6d4997b78de8eef7e0763a990a
mag389 <mag389@cornell.edu>
2020-09-03 03:20:39 +0000

    last part of the project

--- models/amenity.py
+++ models/amenity.py
@@ -7,6 +7,7 @@ from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
+    """ amenity class file """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
 
